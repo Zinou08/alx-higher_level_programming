bb57c09c846bc900d0cfbfe2786a2fdf5a523088
zinou08 <zinebbht08@gmail.com>
2023-06-28 01:35:35 +0000

    Python

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -10,6 +10,7 @@ def safe_print_list_integers(my_list=[], x=0):
     Returns:
         The number of elements printed.
     """
+
     ret = 0
     for i in range(0, x):
         try:
@@ -19,4 +20,3 @@ def safe_print_list_integers(my_list=[], x=0):
             continue
     print("")
     return (ret)
-
